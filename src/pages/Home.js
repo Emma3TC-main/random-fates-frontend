da6696b23643d090990614d490f3dc848669a879
Emma3TC-main <U22329385@utp.edu.pe>
2026-05-02 20:54:38 -0500

    setup CI frontend

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,20 @@
+import { useEffect } from "react";
+import { apiFetch } from "../api/client";
+
 function Home() {
+  useEffect(() => {
+    apiFetch("/api/test")
+      .then((data) => {
+        console.log("API response:", data);
+      })
+      .catch((err) => {
+        console.error("API error:", err);
+      });
+  }, []);
+
   return (
     <div className="p-10 text-center">
       <h2 className="text-2x1/7 font-bold mb-4">Home</h2>
-
-      
     </div>
   );
 }
